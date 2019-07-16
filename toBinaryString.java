0b189d84f0cc5fb2ad9e0a59a16eb547a0b14a19
kimchaeyoung_student <lussi0584@naver.com>
2019-07-16 16:31:39 +0900

    Java practice_file

---
+++ toBinaryString.java
@@ -0,0 +1,14 @@
+
+public class toBinaryString {
+
+	public static void main(String[] args) {
+		int x=4; //y,value;
+		
+		//Integer a=4;	//랩변수(?)
+		
+		System.out.println(x);
+		System.out.println(Integer.toBinaryString(x));	//비트연산한 값을 이진수로 출력하기 위한 것;
+		System.out.println(Integer.toBinaryString(-5));
+	}
+
+}
