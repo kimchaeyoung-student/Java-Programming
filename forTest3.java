556147981546ee2199e29ef3cc4265b68fc9c8a4
kimchaeyoung_student <lussi0584@naver.com>
2019-07-16 16:35:47 +0900

    Java practice_file

---
+++ forTest3.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class forTest3 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> mclass = new ArrayList<>();
+		mclass.add(2015);
+		mclass.add(11);
+		mclass.add(18);
+		
+		for(Integer i :mclass) {
+			System.out.println(i);
+		}
+		//System.out.println(mclass);
+	}
+
+}
