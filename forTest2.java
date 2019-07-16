6149fd480fbae1c636e7d379382bb2daefa02c55
kimchaeyoung_student <lussi0584@naver.com>
2019-07-16 16:35:32 +0900

    Java practice_file

---
+++ forTest2.java
@@ -0,0 +1,11 @@
+
+public class forTest2 {
+
+	public static void main(String[] args) {
+		String mclass[] = {"비트", "자바", "수업"};
+		for(int i=0;i<mclass.length;i++) {
+			System.out.println(mclass[i]);
+		}
+	}
+
+}
