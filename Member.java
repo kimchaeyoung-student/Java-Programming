569711b0070f0908c1869ceb46a0f7a6686db017
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-23 18:45:34 +0900

    Java practice_file

---
+++ Member.java
@@ -0,0 +1,18 @@
+
+public class Member {
+
+	public static void main(String[] args) {
+		String name;
+		String id;
+		String password;
+		int age;
+		
+		name = "±Ë√§øµ";
+		id = "kcy123";
+		password = "12345678";
+		age = 21;
+		
+		System.out.println(name + " " + id + " " + password + " " + age);
+	}
+
+}
