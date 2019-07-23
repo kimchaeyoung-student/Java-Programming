15889f53c14ec9e4da9bb28d01ab5c425c2515d2
kimchaeyoung_student <chaeyoung0584@gmail.com>
2019-07-23 18:45:54 +0900

    Java practice_file

---
+++ SimpleMain.java
@@ -0,0 +1,9 @@
+class SimpleCollector extends SimpleAdapter{
+}
+public class SimpleMain {
+	public static void main(String[] args) {
+		SimpleCollector s = new SimpleCollector();
+		s.printHello();
+	}
+
+}
